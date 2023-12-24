31e9aff203a2f27ccdbbd7ddacc8e6032e35d6c4
Beilak <beylak@yandex.ru>
2023-12-24 13:44:09 +0300

    add dict migration tables

---
+++ db/db_models/db_models.py
@@ -0,0 +1,46 @@
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+
+import sqlalchemy as sa
+
+metadata = sa.MetaData()
+
+## Dict tables
+
+securities_type = sa.Table(
+    "securities_type",
+    metadata,
+    sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid4()),
+    sa.Column("name", sa.String),
+)
+
+sector = sa.Table(
+    "sector",
+    metadata,
+    sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid4()),
+    sa.Column("name", sa.String),
+)
+
+industry = sa.Table(
+    "industry",
+    metadata,
+    sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid4()),
+    sa.Column("name", sa.String),
+)
+
+currency = sa.Table(
+    "currency",
+    metadata,
+    sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid4()),
+    sa.Column("name", sa.String),
+)
+
+currency = sa.Table(
+    "country",
+    metadata,
+    sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid4()),
+    sa.Column("name", sa.String),
+    sa.Column(
+        "currency", UUID(as_uuid=True), sa.ForeignKey("currency.id"), nullable=False
+    ),
+)
